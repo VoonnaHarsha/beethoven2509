f4f2db24ba7637864e79534728f8c83b56e9abe3
VoonnaHarsha <harshavoonna@gmail.com>
2025-09-03 16:39:14 +0530

    03-09-2025

--- 03-09-2025/project_emp/emp/repo.py
+++ 03-09-2025/project_emp/emp/repo.py
@@ -1,4 +1,4 @@
-employees = []
+'''employees = []
 def create_employee(id, name, job_title, salary, join_date):
     emp = {
         'id':id, 
@@ -8,5 +8,26 @@ def create_employee(id, name, job_title, salary, join_date):
         'join_date': join_date
     }
     employees.append(emp)
+def read_all():
+    return employees'''
+
+
+import logging 
+
+logging.basicConfig(filename='app.log', 
+        format='%(asctime)s %(levelname)s: %(message)s',
+        level=logging.INFO)
+
+employees = []
+def create_employee(id, name, job_title, salary, join_date):    
+    emp = {
+        'id':id, 
+        'name': name, 
+        'job_title': job_title, 
+        'salary': salary, 
+        'join_date': join_date
+    }
+    employees.append(emp)
+    logging.info(f'{name} Employee Created.')
 def read_all():
     return employees
